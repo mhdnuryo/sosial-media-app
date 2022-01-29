bda9b2140aa71029ba5fa0511e23ca2e9b3aae06
mhdnuryo <mhdnuryo@gmail.com>
2022-01-30 06:22:30 +0700

    design update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,7 @@ function Login(){
   return (
     <div className="App Login">
       <div className="container container-login">
-        <div className="card" style="width: 18rem;">
+        <div className="card">
           <img src="..." className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
