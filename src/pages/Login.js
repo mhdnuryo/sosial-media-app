c9b892b3d8968a60871c6c0a17126480457b0571
mhdnuryo <mhdnuryo@gmail.com>
2022-01-30 07:26:51 +0700

    design update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,6 +7,10 @@ function Login(){
         <form>
           <fieldset>
             <legend>Login :</legend>
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname" />
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname" />
           </fieldset>
         </form>
       </div>
