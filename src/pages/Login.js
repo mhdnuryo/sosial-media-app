30f432c4d037f57d8b23cf24665e2c308bdb2f39
mhdnuryo <mhdnuryo@gmail.com>
2022-01-30 08:22:16 +0700

    design update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,16 +3,26 @@ import React from 'react';
 function Login(){
   return (
     <div className="App Login">
-      <div className="container container-login">
-        <form>
-          <fieldset>
-            <legend>Login :</legend>
-            <label for="fname">First name:</label>
-            <input type="text" id="fname" name="fname" />
-            <label for="lname">Last name:</label>
-            <input type="text" id="lname" name="lname" />
-          </fieldset>
-        </form>
+      <div className"container container-login">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Login</h5>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">
+                  <i class="fas fa-user-circle"></i>
+                </span>
+              </div>
+              <input 
+                type="text"
+                class="form-control" 
+                placeholder="Username" 
+                aria-label="Username" 
+                aria-describedby="basic-addon1"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
