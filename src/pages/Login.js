7f545b21371c4d0d253170e8dde04a0c0a1f9c3a
mhdnuryo <mhdnuryo@gmail.com>
2022-01-30 07:08:01 +0700

    design update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,14 +4,12 @@ function Login(){
   return (
     <div className="App Login">
       <div className="container container-login">
-        <div className="card">
-          <img src="https://tinyurl.com/yd6t3vnw" className="card-img-top img-fluid" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">.............</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
+        <form>
+            <fieldset>
+              <legend>Login :</legend>
+            </fieldset>
+          </legend>
+        </form>
       </div>
     </div>
   )
