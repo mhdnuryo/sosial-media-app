f824ae50e5d73de5bdfaceb1ea02b0889aa31a49
mhdnuryo <mhdnuryo@gmail.com>
2022-01-30 10:25:07 +0700

    design update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,15 +3,7 @@ import React from 'react';
 function Login(){
   return (
     <div className="App Login">
-      <form action="/action_page.php">
-        <fieldset>
-          <legend>Login:</legend>
-          <label for="fname">First name:</label>
-          <input type="text" id="fname" name="fname"><br/><br/>
-          <label for="lname">Last name:</label>
-           <input type="text" id="lname" name="lname"><br/><br/>
-        </fieldset>
-      </form> 
+       <div class="container container-login"></div>
     </div>
   )
 }
