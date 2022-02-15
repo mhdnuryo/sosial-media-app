486e988fdd1fa42d5f292065adc3852e0ee955d7
mhdnuryo <mhdnuryo@gmail.com>
2022-02-16 00:21:52 +0700

    update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import useSocket from '../custom_hooks/useSocket';
 
 function About(){
+  var [socket] = useSocket(process.env)
+
   return (
     <div className="App About">
       <h2>About</h2>
