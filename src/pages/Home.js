c8ae3ffa73468f4e838b984e30db711651134214
mhdnuryo <mhdnuryo@gmail.com>
2022-02-21 16:23:51 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,9 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Search from '../components/Search';
 import useSocket from '../custom_hooks/useSocket';
-import * as redux from 'react-redux';
-import * as Router from 'react-router-dom';
-import * as user from '../redux/reducers/user'
 
 function Home({value}){
   var userId = React.createRef()
-  var dispatch = redux.useDispatch()
   var api = process.env.REACT_APP_API
   var [socket] = useSocket(api,value)
 
