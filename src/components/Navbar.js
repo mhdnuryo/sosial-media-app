919159ea07b481dcdeff0cf1d858604da27fd067
mhdnuryo <mhdnuryo@gmail.com>
2022-02-22 06:15:45 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import Dropdown from './dropdown'
+import Dropdown from './Dropdown'
 
 function Navbar(){
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <a href="#" className="brand-logo logo left">
-          App
-        </a>
-        <ul id="nav-mobile" className="right">
-          <li>
-            <a href="#" className="dropdown-trigger" data-target="dropdown">
-              <i className="material-icons">arrow_drop_down</i>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <Dropdown />
+    <>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo logo left">
+            App
+          </a>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <a href="#" className="dropdown-trigger" data-target="dropdown">
+                <i className="material-icons">arrow_drop_down</i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Dropdown />
+    </>
   )
 }
 
