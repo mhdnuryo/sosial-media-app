87a9b2f663c3ed17c8d8ddbb94ac62a5142d246b
mhdnuryo <mhdnuryo@gmail.com>
2022-02-21 17:20:16 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
     <nav>
       <div className="nav-wrapper">
         <a href="#" className="brand-logo">Logo</a>
-        <ul id="nav-mobile">
+        <ul id="nav-mobile" className"right">
           <li><a href="sass.html">Sass</a></li>
           <li><a href="badges.html">Components</a></li>
           <li><a href="collapsible.html">JavaScript</a></li>
