230a4596e560edb992c17da856319940917d6197
mhdnuryo <mhdnuryo@gmail.com>
2022-02-21 17:29:34 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ function Navbar(){
   return (
     <nav>
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">Logo</a>
+        <a href="#" className="brand-logo">
+          <img src="https://materializecss.com/images/yuna.jpg" class="cirlce responsive-img" />
+        </a>
         <ul id="nav-mobile" className="right">
           <li><a href="sass.html">Sass</a></li>
           <li><a href="badges.html">Components</a></li>
