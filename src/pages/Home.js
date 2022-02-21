14604a282f1ea7e40bc1edb8b894a58768797e1b
mhdnuryo <mhdnuryo@gmail.com>
2022-02-21 16:25:27 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import Search from '../components/Search';
 import useSocket from '../custom_hooks/useSocket';
 
 function Home({value}){
-  var userId = React.createRef()
   var api = process.env.REACT_APP_API
   var [socket] = useSocket(api,value)
 
